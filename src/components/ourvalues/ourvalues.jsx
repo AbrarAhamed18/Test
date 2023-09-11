9af79059bc7be4a4352113f5ffdcbe9f1d042617
AbrarAhamed18 <abrar18ahamedz@gmail.com>
2023-09-11 11:56:43 +0530

    first commt2

--- src/components/ourvalues/ourvalues.jsx
+++ src/components/ourvalues/ourvalues.jsx
@@ -9,7 +9,7 @@ import 'aos/dist/aos.css'
 
 const Ourvalues = () => {
     useEffect(()=>{
-        Aos.init({duration: 1000})
+        Aos.init({duration: 500})
     },[])
   return (
     <section id='section3' className='ourval'>
@@ -20,38 +20,38 @@ const Ourvalues = () => {
             <p className='bottomText'>the best people to unleash their creativity. </p>
             </div>
             <div className="firstContainer">
-                <div className="leftText">
-                    <h1 data-aos='fade-up' className='Explore'>Explore <br/>
+                <div data-aos='fade-up' className="leftText">
+                    <h1  className='Explore'>Explore <br/>
                     Together.</h1>
-                    <p data-aos='fade-up'>Game making is about exploration and facing uncertainty, together. High ambitions require humility about what we don’t know. That is why we have created an environment where our talents constantly support each other and where passion & learnings are shared.</p>
+                    <p >Game making is about exploration and facing uncertainty, together. High ambitions require humility about what we don’t know. That is why we have created an environment where our talents constantly support each other and where passion & learnings are shared.</p>
                 </div>
                 <div className="rightpic">
-                    <img data-aos='zoom-in' className='rpic1' src={rimg1} alt="" />
+                    <img data-aos='zoom-in'  className='rpic1' src={rimg1} alt="" />
                 </div>
             </div>
             <div className="secondContainer">
                 <div className="leftpic">
-                <img data-aos='zoom-in' className='rpic1' src={limg1} alt="" />
+                <img data-aos='zoom-in'  className='rpic1' src={limg1} alt="" />
                     
                 </div>
-                <div className="rightText">
-                <h1 data-aos='fade-up' className='Learn'>Learn Fast.</h1>
-                    <p data-aos='fade-up'>
+                <div data-aos='fade-up' className="rightText">
+                <h1  className='Learn'>Learn Fast.</h1>
+                    <p>
 Assessing our ideas through high-speed testing, and receiving feedback as fast and often as we can is paramount at Madbox. Quick failure and iteration is always preferred to long-drawn-out discussions.</p>                    
                 </div>
             </div>
             <div className="thirdContainer">
-                <div className="leftText">
-                    <h1 data-aos='fade-up'  className='Be'>Be Thoughtfully Candid.</h1>
-                    <p data-aos='fade-up'>At Madbox, anything should be said as long as it is thoughtfully shared. We believe that the delivery of feedback is as equally important as the message itself. This is our way to build a transparent culture, where everyone participates in both the celebrations and challenges of building a company together.</p>
+                <div data-aos='fade-up' className="leftText">
+                    <h1  className='Be'>Be Thoughtfully Candid.</h1>
+                    <p >At Madbox, anything should be said as long as it is thoughtfully shared. We believe that the delivery of feedback is as equally important as the message itself. This is our way to build a transparent culture, where everyone participates in both the celebrations and challenges of building a company together.</p>
                 </div>
                 <div className="rightpic">
-                    <img data-aos='zoom-in'  className='rpic1' src={rimg2} alt="" />
+                    <img data-aos='zoom-in' className='rpic1' src={rimg2} alt="" />
                 </div>
             </div>
             <div className="fourthContainer">
                 <div className="leftpic">
-                <img data-aos='zoom-in' className='rpic1' src={limg2} alt="" />
+                <img data-aos='zoom-in'  className='rpic1' src={limg2} alt="" />
                     
                 </div>
                 <div data-aos='fade-up' className="rightText">
