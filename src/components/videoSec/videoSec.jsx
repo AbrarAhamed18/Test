1ab3c1f54aee7da0932bc6e6fe5e00f4be204a20
AbrarAhamed18 <abrar18ahamedz@gmail.com>
2023-09-11 15:13:50 +0530

    commit

--- src/components/videoSec/videoSec.jsx
+++ src/components/videoSec/videoSec.jsx
@@ -1,26 +1,62 @@
-import React,{useEffect} from 'react'
-import video from '../../Assets/hero1.mp4'
-import './videoSec.css'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+
+
+import React, { useEffect, useRef } from 'react';
+import video from '../../Assets/hero1.mp4';
+import './videoSec.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const VideoSec = () => {
+  const videoRef = useRef(null);
 
-  useEffect(()=>{
-    Aos.init({duration: 2000})
-},[])
-    
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+
+    // Intersection Observer callback
+    const handleIntersection = (entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting) {
+        // Video is in view, play it
+        videoRef.current.play();
+      } else {
+        // Video is out of view, pause it
+        videoRef.current.pause();
+      }
+    };
+
+    // Create an Intersection Observer instance
+    const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
+    // Observe the video element
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    // Cleanup: disconnect the observer when the component unmounts
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
+
   return (
     <section className='videoSec'>
-        {/* <div className="videoTalk"></div> */}
-           
-            <video data-aos='zoom-in' className='videoFolder' src={video} muted autoPlay loop type="video/mp4" ></video>
-            <div className="vText">
-                <h1 data-aos='slide-up'>Unleash Your Creativity! </h1>
-            </div>
-        
+      <video
+        data-aos='zoom-in'
+        className='videoFolder'
+        src={video}
+        muted
+        autoPlay
+        loop
+        type='video/mp4'
+        ref={videoRef} // Reference to the video element
+      ></video>
+      <div className='vText'>
+        <h1 data-aos='slide-up'>Unleash Your Creativity! </h1>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default VideoSec
+export default VideoSec;
