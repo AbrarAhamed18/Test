1ee6a8c26be9b9a36c52f0cb748856493b9e849a
AbrarAhamed18 <abrar18ahamedz@gmail.com>
2023-09-14 11:44:02 +0530

    final c

--- src/components/Staticfooter/Staticfooter.jsx
+++ src/components/Staticfooter/Staticfooter.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+
+
+import React, { useState } from 'react';
 import './staticfooter.css';
 
 function Staticfooter() {
+  const [activeButton, setActiveButton] = useState(null);
+
   const handleNavigation = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      setActiveButton(sectionId); // Set the active button
     }
   };
 
   return (
     <footer>
       <div className="footer-links">
-        <button onClick={() => handleNavigation('section1')}>A Casually Mad Career</button>
-        <button onClick={() => handleNavigation('section2')}>Inside MADBOX</button>
-        <button onClick={() => handleNavigation('section3')}>Our Values</button>
-        <button onClick={() => handleNavigation('section4')}>Power up with Benefits.</button>
-        <button onClick={() => handleNavigation('section5')}>Our Story</button>
+        <button
+          onClick={() => handleNavigation('section1')}
+          className={activeButton === 'section1' ? 'active' : ''}
+        >
+          A Casually Mad Career
+        </button>
+        <button
+          onClick={() => handleNavigation('section2')}
+          className={activeButton === 'section2' ? 'active1' : ''}
+        >
+          Inside MADBOX
+        </button>
+        <button
+          onClick={() => handleNavigation('section3')}
+          className={activeButton === 'section3' ? 'active' : ''}
+        >
+          Our Values
+        </button>
+        <button
+          onClick={() => handleNavigation('section4')}
+          className={activeButton === 'section4' ? 'active2' : ''}
+        >
+          Power up with Benefits.
+        </button>
+        <button
+          onClick={() => handleNavigation('section5')}
+          className={activeButton === 'section5' ? 'active' : ''}
+        >
+          Our Story
+        </button>
       </div>
     </footer>
   );
